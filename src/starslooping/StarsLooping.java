430f187750bd8316dc8af662c9a00aef0b3cfdc0
riorenata <riorenata.id@gmail.com>
2022-08-13 03:58:23 +0700

    first commit

---
+++ src/starslooping/StarsLooping.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package starslooping;
+
+/**
+ *
+ * @author Raze
+ */
+public class StarsLooping {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        form1 f = new form1();
+        f.show();
+    }
+    
+}
